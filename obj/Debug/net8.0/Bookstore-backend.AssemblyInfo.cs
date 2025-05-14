26b8f1ad4411bb9c26d24ffe16e2d8cd9aa8d816
nitikasuwal <nitikasuwal0@gmail.com>
2025-05-14 11:42:38 +0545

    Added review feature and add rating feature in backend

--- obj/Debug/net8.0/Bookstore-backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/Bookstore-backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bookstore-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af43f497f9bcdd457a82a1033f6108c3256e71d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bookstore-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bookstore-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
